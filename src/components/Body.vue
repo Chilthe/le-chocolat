f44cea7dc9e0a8f4d474c9e29c2c2291bd3145d5
chilthe <theo.chilinski@objectware.fr>
2025-10-31 10:44:14 +0100

    Add display row for Body

--- src/components/Body.vue
+++ src/components/Body.vue
@@ -1,11 +1,12 @@
 <template>
   <main class="flex-1">
     <Offer v-if="OFFER" />
-    <div v-for="product in products" :key="product.id">
-      <img 
-        :src="getImageUrl(product.img)"
-        :alt="product.title"
-      />
+    <div class="grid grid-cols-3">
+      <div v-for="product in products" :key="product.id">
+        <div class="pa-1 col-span-1">
+          <img :src="getImageUrl(product.img)" :alt="product.title" />
+        </div>
+      </div>
     </div>
   </main>
 </template>
@@ -18,6 +19,6 @@ import Offer from "~/Offer.vue";
 const OFFER = ref(true);
 
 const getImageUrl = (filename) => {
-  return new URL(`../assets/${filename}`, import.meta.url).href
-}
+  return new URL(`../assets/${filename}`, import.meta.url).href;
+};
 </script>
