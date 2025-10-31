f99d0a626585115e26d45160d5db271917d0fa57
chilthe <theo.chilinski@objectware.fr>
2025-10-31 10:44:46 +0100

    Update header style w/ buttons, icons, scroll behavior

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,30 +1,44 @@
 <template>
   <header
-    :class="`h-20 w-full ${isFixed ? 'fixed' : 'relative'} 
-      bg-primary${isFixed ? '' : '/0'}
-      transition-all duration-500 ease-in-out z-1`"
+    :class="`h-20 w-full fixed bg-primary transition-all duration-150 ease-in-out z-1 
+    ${isVisible ? 'translate-y-0' : '-translate-y-full'}`"
   >
     <div class="h-full grid grid-cols-3 items-center">
-      <div class="col-span-1 pl-25 flex justify-start bg-sky-400">
-        <img src="./in.png" />
+      <div class="col-span-1 pl-25 flex justify-start">
+        <button @click="test('menu')">
+          <Bars3Icon class="size-10" />
+        </button>
       </div>
 
-      <div class="col-span-1 flex justify-center bg-pink-400">
-        <strong style="font-size: x-large">LE CHOCOLAT</strong>
+      <div class="col-span-1 flex justify-center">
+        <strong class="text-3xl">LE CHOCOLAT</strong>
       </div>
 
-      <div class="col-span-1 pr-25 flex justify-end bg-sky-400">
-        <img src="./in.png" />
-        <img src="./in.png" />
+      <div class="col-span-1 pr-25 flex justify-end">
+        <button @click="test('search')">
+          <MagnifyingGlassIcon class="size-8" />
+        </button>
+        <button @click="test('user')">
+          <UserIcon class="size-8 mx-3" />
+        </button>
+        <button @click="test('shop')">
+          <ShoppingBagIcon class="size-8" />
+        </button>
       </div>
     </div>
   </header>
 </template>
 
 <script setup>
-import { ref, onMounted, onUnmounted } from "vue";
+import { ref, onMounted, onUnmounted, watch } from "vue";
+import {
+  Bars3Icon,
+  MagnifyingGlassIcon,
+  ShoppingBagIcon,
+  UserIcon,
+} from "@heroicons/vue/24/solid";
 
-const isFixed = ref(true);
+const isVisible = ref(true);
 const lastScrollY = ref(0);
 
 onMounted(() => {
@@ -39,17 +53,31 @@ const handleScroll = () => {
   const currentScrollY = window.scrollY;
 
   if (currentScrollY > lastScrollY.value) {
-    isFixed.value = false;
+    isVisible.value = false;
   } else {
-    isFixed.value = true;
+    isVisible.value = true;
   }
 
   lastScrollY.value = currentScrollY;
 };
+
+// Write components :
+// - menu
+// - search
+// - user
+// - shopping cart
+function test(str) {
+  alert(str);
+  return;
+}
 </script>
 
 <style>
 body {
   scroll-behavior: smooth;
+}
+
+button {
+  cursor: pointer;
 }
 </style>
