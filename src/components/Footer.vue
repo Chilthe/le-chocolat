7abbf41d27673338a275fb9315f6ba2c442bba4b
Chilth <theo.chilinski@gmail.com>
2025-11-04 16:45:59 +0100

    Update Footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,14 +1,26 @@
 <template>
-  <footer class="h-50 flex justify-center items-center">
-    <div class="w-full h-full flex justify-center items-center">
-      <div>
-        <p>CONTACT</p>
-        <p>Thomas Delannoy</p>
-        <p>Chef Chocolatier - confiseur</p>
-        <p>+33 (0)6 11 30 73 61</p>
-        <p>tdel@augusteetferdinand.com</p>
-        <p>augusteetferdinand.com</p>
-      </div>
+  <footer>
+    <div class="w-full h-50 flex justify-center items-center">
+      <menu>
+        <li>CONTACT</li>
+        <li>Thomas Delannoy</li>
+        <li>Chef Chocolatier - confiseur</li>
+        <li>+33 (0)6 11 30 73 61</li>
+        <li>tdel@augusteetferdinand.com</li>
+        <li>augusteetferdinand.com</li>
+      </menu>
+
+      <!-- Add router -->
+      <menu>
+        <li>NOS PRODUITS</li>
+        <li>Chocolats</li>
+        <li>Confiseries</li>
+        <li>Éditions limitées</li>
+      </menu>
+    </div>
+    <div class="h-25 w-full flex justify-center items-center">
+      <!-- same as header -->
+      <strong class="text-4xl">LE CHOCOLAT</strong>
     </div>
   </footer>
 </template>
